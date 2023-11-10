a20151ae18297818cd3ee3a4e2b5fdbf496a857d
Aryan Sethi <aryan.rdps@gmail.com>
2023-11-10 09:02:31 +0530

    Did q5

---
+++ q5/index.js
@@ -0,0 +1,11 @@
+const myPara = document.querySelector(".para");
+
+const changeColor = color => {
+    myPara.style.color = color;
+};
+const changeFamily = fam => {
+    myPara.style.fontFamily = fam;
+};
+const changeFont = font => {
+    myPara.style.fontSize = font;
+};
